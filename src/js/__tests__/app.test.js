7d1707f2d7e95e3e03a959e2a7587b8eac188abc
Svetlana Kutyova <kutyovas@yandex.ru>
2021-11-30 22:46:40 +0300

    2

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,6 +2,6 @@ import GameSavingLoader from '../GameSavingLoader.js';
 
 test('the data is buffer', () => {
   GameSavingLoader.load().then((saving) => {
-    expect(saving).toEqual(/{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}/);
+    expect(saving).toEqual('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
   });
 });
