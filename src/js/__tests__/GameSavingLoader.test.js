62eb2927b10b97f3cf93f049592a9c5cd3c08272
Svetlana Kutyova <kutyovas@yandex.ru>
2021-12-02 22:45:44 +0300

    2

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,6 +1,6 @@
 import GameSavingLoader from '../GameSavingLoader.js';
 
-test('the return saving', () => GameSavingLoader.load().then((saving) => {
+test('the return saving', () => {
   const savingObj = {
     id: 9,
     created: 1546300800,
@@ -11,11 +11,11 @@ test('the return saving', () => GameSavingLoader.load().then((saving) => {
       points: 2000,
     },
   };
-  return expect(saving).not.toEqual(savingObj);
+  return GameSavingLoader.load().then((saving) => {
+    expect(saving).not.toEqual(savingObj);
+  });
+});
+
+test('the data saving2', () => GameSavingLoader.load().then((saving) => {
+  expect(saving).not.toBe(JSON.parse('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}'));
 }));
-
-test('the data saving', () => GameSavingLoader.load().then((saving) => expect(saving).not.toEqual(JSON.parse('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}'))));
-
-/*
-  expect(Promise.resolve(savingObj)).resolves.toEqual(savingObj);
-*/
